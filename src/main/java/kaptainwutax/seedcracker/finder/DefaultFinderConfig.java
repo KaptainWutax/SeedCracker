7a49baa19ca6770a49ecee5fd0084555ecdad15c
Unknown <kaptainwutax@gmail.com>
2019-12-14 16:45:28 -0500

    removed redundant super

--- src/main/java/kaptainwutax/seedcracker/finder/DefaultFinderConfig.java
+++ src/main/java/kaptainwutax/seedcracker/finder/DefaultFinderConfig.java
@@ -3,7 +3,6 @@ package kaptainwutax.seedcracker.finder;
 public class DefaultFinderConfig extends FinderConfig {
 
     public DefaultFinderConfig() {
-        super();
         this.typeStates.put(Type.DIAMOND_ORE, false);
         this.typeStates.put(Type.INFESTED_STONE_ORE, false);
         this.typeStates.put(Type.IGLOO, false);
