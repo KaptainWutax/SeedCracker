15b4601e95c76a0b55c02e37d6d2e3de0457c262
Unknown <kaptainwutax@gmail.com>
2020-01-30 13:25:08 -0500

    clear seed crack input earlier

--- src/main/java/kaptainwutax/seedcracker/mixin/ClientWorldMixin.java
+++ src/main/java/kaptainwutax/seedcracker/mixin/ClientWorldMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ClientWorld.class)
 public abstract class ClientWorldMixin {
 
-    @Inject(method = "disconnect", at = @At("TAIL"))
+    @Inject(method = "disconnect", at = @At("HEAD"))
     private void disconnect(CallbackInfo ci) {
         SeedCracker.get().clear();
         FinderQueue.get().clear();
