d147e0233e3e6053498e9cb237b1d0493b87fd1f
KaptainWutax <kaptainwutax@gmail.com>
2020-04-30 16:14:53 -0400

    turned off dungeons by default and 0.1.0 release

--- src/main/java/kaptainwutax/seedcracker/finder/profile/VanillaProfile.java
+++ src/main/java/kaptainwutax/seedcracker/finder/profile/VanillaProfile.java
@@ -1,11 +1,15 @@
 package kaptainwutax.seedcracker.finder.profile;
 
+import kaptainwutax.seedcracker.finder.Finder;
+
 public class VanillaProfile extends FinderProfile {
 
 	public VanillaProfile() {
 		super(true);
 		this.author = "KaptainWutax";
 		this.locked = false;
+
+		this.setTypeState(Finder.Type.DUNGEON, false);
 	}
 
 }
